503a715e29362e24d9c2fc1952e348193bdb9f57
Joseph <hwansmom0622@gmail.com>
2019-03-15 18:43:31 -0400

    add notebook

---
+++ turtleModule.py
@@ -0,0 +1,10 @@
+import turtle
+
+def drawTriangle(t, sz):
+    for i in range(3):
+        alex.forward(sz)
+        alex.left(120)
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+drawTriangle(alex, 90)
